a95458827395c460f27ba2f172857049fff879ae
Thiago Uriel <thiago.uriel@gmail.com>
2024-07-23 19:51:43 -0700

    Reviews and adjustments in the Consumables feature.

---
+++ Source/NinjaGAS/Private/NinjaGASFunctionLibrary.cpp
@@ -0,0 +1,21 @@
+﻿// Ninja Bear Studio Inc., all rights reserved.
+#include "NinjaGASFunctionLibrary.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+int32 UNinjaGASFunctionLibrary::SendGameplayEventToActor(const AActor* AbilityOwner, const FGameplayTag EventTag, const FGameplayEventData& EventData)
+{
+	UAbilitySystemComponent* AbilityComponent = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(AbilityOwner);
+	return SendGameplayEventToComponent(AbilityComponent, EventTag, EventData);
+}
+
+int32 UNinjaGASFunctionLibrary::SendGameplayEventToComponent(UAbilitySystemComponent* AbilityComponent, const FGameplayTag EventTag, const FGameplayEventData& EventData)
+{
+	if (!EventTag.IsValid() || !IsValid(AbilityComponent))
+	{
+		return 0;
+	}
+	
+	return AbilityComponent->HandleGameplayEvent(EventTag, &EventData);	
+}
---
+++ Source/NinjaGAS/Public/NinjaGASFunctionLibrary.h
@@ -0,0 +1,43 @@
+﻿// Ninja Bear Studio Inc., all rights reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "NinjaGASFunctionLibrary.generated.h"
+
+/**
+ * Helper functions for the Ninja GAS system.
+ */
+UCLASS()
+class NINJAGAS_API UNinjaGASFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	
+	GENERATED_BODY()
+
+public:
+
+	/**
+	 * Sends a Gameplay Event to the Owner's of an Ability System Component.
+	 *
+	 * @param AbilityOwner		Owner of the Ability System Component.
+	 * @param EventTag			Gameplay Tag identifying the Gameplay Event.
+	 * @param EventData			Payload for the Gameplay Event.
+	 * @return					The number of successful ability activations triggered by the event.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Ninja GAS", meta = (DefaultToSelf = "AbilityOwner", ReturnDisplayName = "Activations"))
+	static int32 SendGameplayEventToActor(const AActor* AbilityOwner, const FGameplayTag EventTag, const FGameplayEventData& EventData);
+
+	/**
+	 * Sends a Gameplay Event to the Ability System Component.
+	 *
+	 * @param AbilityComponent	Ability Component receiving the event.
+	 * @param EventTag			Gameplay Tag identifying the Gameplay Event.
+	 * @param EventData			Payload for the Gameplay Event.
+	 * @return					The number of successful ability activations triggered by the event.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Ninja GAS", meta = (ReturnDisplayName = "Activations"))
+	static int32 SendGameplayEventToComponent(UAbilitySystemComponent* AbilityComponent, const FGameplayTag EventTag, const FGameplayEventData& EventData);
+	
+};
