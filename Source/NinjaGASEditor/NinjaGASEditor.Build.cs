a0dc9fb35f8abd6937d957f0baad77f24c642f14
Thiago Uriel <thiago.uriel@gmail.com>
2024-10-03 22:09:55 -0700

    [improvement] Organized asset categories for NBS.

---
+++ Source/NinjaGASEditor/NinjaGASEditor.Build.cs
@@ -0,0 +1,31 @@
+﻿// Ninja Bear Studio Inc., all rights reserved. 
+// ReSharper disable InconsistentNaming
+using UnrealBuildTool;
+
+public class NinjaGASEditor : ModuleRules
+{
+    public NinjaGASEditor(ReadOnlyTargetRules target) : base(target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+	        new []
+	        {
+		        "Core", 
+		        "NinjaGAS"
+	        }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new []
+            {
+	            "AssetTools",
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "UnrealEd"
+            }
+        );
+    }
+}
