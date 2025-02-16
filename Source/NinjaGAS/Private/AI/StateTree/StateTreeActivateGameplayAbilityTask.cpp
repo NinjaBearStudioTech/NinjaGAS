4f97e4b2d14f45bec47ef7bcff5a87f0ecf4169a
Thiago Uriel <thiago.uriel@gmail.com>
2025-02-15 23:33:20 -0800

    [added] State Tree task to Activate Gameplay Abilities.

---
+++ Source/NinjaGAS/Private/AI/StateTree/StateTreeActivateGameplayAbilityTask.cpp
@@ -0,0 +1,127 @@
+﻿// Ninja Bear Studio Inc., all rights reserved.
+#include "AI/StateTree/StateTreeActivateGameplayAbilityTask.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "AIController.h"
+#include "StateTreeExecutionContext.h"
+#include "Abilities/GameplayAbility.h"
+#include "Runtime/Launch/Resources/Version.h"
+
+EStateTreeRunStatus FStateTreeActivateGameplayAbilityTask::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	if (!InstanceData.AIController)
+	{
+		UE_VLOG(Context.GetOwner(), LogStateTree, Error, TEXT("FStateTreeActivateGameplayAbilityTask failed since AIController is missing."));
+		return EStateTreeRunStatus::Failed;
+	}
+
+	const APawn* Pawn = InstanceData.AIController->GetPawn();
+	UAbilitySystemComponent* AbilityComponent = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Pawn);
+	if (!AbilityComponent)
+	{
+		UE_VLOG(Context.GetOwner(), LogStateTree, Error, TEXT("FStateTreeActivateGameplayAbilityTask failed since Pawn does not have an ASC."));
+		return EStateTreeRunStatus::Failed;
+	}
+
+	return ActivateAbility(Context, AbilityComponent);
+}
+
+EStateTreeRunStatus FStateTreeActivateGameplayAbilityTask::ActivateAbility(const FStateTreeExecutionContext& Context, UAbilitySystemComponent* AbilityComponent) const
+{
+	if (!IsValid(AbilityComponent))
+	{
+		UE_VLOG(Context.GetOwner(), LogStateTree, Error, TEXT("FStateTreeActivateGameplayAbilityTask failed since an invalid ASC was received."));
+		return EStateTreeRunStatus::Failed;
+	}
+	
+	const FDelegateHandle Handle = AbilityComponent->OnAbilityEnded.AddLambda([this, InstanceDataRef = Context.GetInstanceDataStructRef(*this)](const FAbilityEndedData& AbilityEndedData) mutable
+	{
+		FInstanceDataType* InstanceDataPtr = InstanceDataRef.GetPtr();
+		if (InstanceDataPtr && CheckAbilityThatHasEnded(InstanceDataPtr, AbilityEndedData))
+		{
+			InstanceDataPtr->bAbilityHasEnded = true;
+			InstanceDataPtr->AbilitySpec = AbilityEndedData.AbilitySpecHandle;
+			InstanceDataPtr->bAbilityWasCancelled = AbilityEndedData.bWasCancelled;
+		}
+	});
+
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	InstanceData.AbilityEndedHandle = Handle;
+	
+	const FGameplayTagContainer AbilityTriggerTags = Context.GetInstanceData(*this).AbilityActivationTags;
+	UE_VLOG(Context.GetOwner(), LogStateTree, Log, TEXT("FStateTreeActivateGameplayAbilityTask will activate ability using %s."), *AbilityTriggerTags.ToStringSimple());
+	
+	AbilityComponent->TryActivateAbilitiesByTag(AbilityTriggerTags);
+	return EStateTreeRunStatus::Running;
+}
+
+bool FStateTreeActivateGameplayAbilityTask::CheckAbilityThatHasEnded(const FInstanceDataType* InstanceData, const FAbilityEndedData& AbilityEndedData) const
+{
+	FGameplayTagContainer AbilityThatEndedTags = FGameplayTagContainer();
+
+#if ENGINE_MINOR_VERSION < 5
+	AbilityThatEndedTags.AppendTags(Data.AbilityThatEnded->AbilityTags);
+#else
+	AbilityThatEndedTags.AppendTags(AbilityEndedData.AbilityThatEnded->GetAssetTags());
+#endif
+
+	const FGameplayTagContainer AbilityActivationTags = InstanceData->AbilityActivationTags;
+	return AbilityThatEndedTags.HasAll(AbilityActivationTags);
+}
+
+EStateTreeRunStatus FStateTreeActivateGameplayAbilityTask::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	EStateTreeRunStatus Status = EStateTreeRunStatus::Running;
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	
+	if (InstanceData.bAbilityHasEnded)
+	{
+		UE_VLOG(Context.GetOwner(), LogStateTree, Log,
+			TEXT("FStateTreeActivateGameplayAbilityTask has received ability status: %s: %s."),
+			*InstanceData.AbilityActivationTags.ToStringSimple(),
+			InstanceData.bAbilityWasCancelled ? TEXT("been cancelled") : TEXT("ended"));
+
+		// Check what is the correct status, based on the parameters assigned to the instance.
+		if (InstanceData.bShouldFinishStateWhenAbilityCompletes)
+		{
+			Status = InstanceData.bAbilityWasCancelled || !InstanceData.bTreatCancelledAbilityAsSuccess
+				? EStateTreeRunStatus::Failed
+				: EStateTreeRunStatus::Succeeded;
+		}
+	}
+	
+	return Status;
+}
+
+void FStateTreeActivateGameplayAbilityTask::ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	if (!IsValid(InstanceData.AIController))
+	{
+		UE_VLOG(Context.GetOwner(), LogStateTree, Warning, TEXT("FStateTreeActivateGameplayAbilityTask AIController is invalid in ExitState."));
+		return;
+	}
+
+	const APawn* Pawn = InstanceData.AIController->GetPawn();
+	UAbilitySystemComponent* AbilityComponent = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Pawn);
+
+	if (IsValid(AbilityComponent) && InstanceData.AbilityEndedHandle.IsValid())
+	{
+		// Remove the delegate first to avoid notifications when forcibly ending the ability.
+		AbilityComponent->OnAbilityEnded.Remove(InstanceData.AbilityEndedHandle);
+		InstanceData.AbilityEndedHandle.Reset();
+
+		// Force ability cancellation if it hasn't already ended and should be stopped when the state finishes.
+		if (!InstanceData.bAbilityHasEnded && InstanceData.bShouldEndAbilityWhenStateFinishes)
+		{
+			UE_VLOG(Context.GetOwner(), LogStateTree, Log,
+				TEXT("FStateTreeActivateGameplayAbilityTask forcing cancellation of ability activated by %s."),
+				*InstanceData.AbilityActivationTags.ToStringSimple());
+
+			AbilityComponent->CancelAbilityHandle(InstanceData.AbilitySpec);
+		}
+	}
+}
---
+++ Source/NinjaGAS/Public/AI/StateTree/StateTreeActivateGameplayAbilityTask.h
@@ -0,0 +1,89 @@
+﻿// Ninja Bear Studio Inc., all rights reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "StateTreeTaskBase.h"
+#include "StateTreeActivateGameplayAbilityTask.generated.h"
+
+class AAIController;
+
+USTRUCT()
+struct FStateTreeActivateGameplayAbilityTaskInstanceData
+{
+	
+	GENERATED_BODY()
+
+	/** The AI Controller that is running the State Tree. */
+	UPROPERTY(EditAnywhere, Category = Context)
+	TObjectPtr<AAIController> AIController = nullptr;
+
+	/** Gameplay Tags used to activate the ability. */
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	FGameplayTagContainer AbilityActivationTags = FGameplayTagContainer::EmptyContainer;
+
+	/** Determines if a cancelled ability should be handled as success. */
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	bool bTreatCancelledAbilityAsSuccess = true;
+
+	/** If set to true, finishes the task/state once the ability ends. */
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	bool bShouldFinishStateWhenAbilityCompletes = true;
+
+	/** If set to true, cancels the ability when the state changes. */
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	bool bShouldEndAbilityWhenStateFinishes = true;
+	
+	/** Spec for the Gameplay Ability that has Ended. */
+	UPROPERTY(EditAnywhere, Category = Output)
+	FGameplayAbilitySpecHandle AbilitySpec = FGameplayAbilitySpecHandle();
+
+	/** Informs when the ability has ended. */
+	UPROPERTY(EditAnywhere, Category = Output)
+	bool bAbilityHasEnded = false;
+	
+	/** Informs if this ability was cancelled. */
+	UPROPERTY(EditAnywhere, Category = Output)
+	bool bAbilityWasCancelled = false;
+
+	/** Delegate Handle provided by the ASC. */
+	FDelegateHandle AbilityEndedHandle;
+};
+
+/**
+ * Activates a Gameplay Ability and completes the state when the ability finishes.
+ */
+USTRUCT(meta = (DisplayName = "Activate Gameplay Ability", Category = "G.A.S."))
+struct NINJAGAS_API FStateTreeActivateGameplayAbilityTask : public FStateTreeTaskCommonBase
+{
+	
+	GENERATED_BODY()
+
+	using FInstanceDataType = FStateTreeActivateGameplayAbilityTaskInstanceData;
+	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+
+protected:
+
+	/**
+	 * Activates the ability requested in the context.
+	 *
+	 * @param Context				Context providing activation info.
+	 * @param AbilityComponent		Ability System Component that will activate the ability.
+	 * @return						True if the activation is successful.
+	 */
+	virtual EStateTreeRunStatus ActivateAbility(const FStateTreeExecutionContext& Context, UAbilitySystemComponent* AbilityComponent) const;
+
+	/**
+	 * Checks the ability that has ended to match if it's the one we are trying to activate.
+	 *
+	 * @param InstanceData			Access to the instance data backing this task.
+	 * @param AbilityEndedData		Information about the Gameplay Ability that has ended.
+	 * @return						True if this is the same ability that this task activated.
+	 */
+	virtual bool CheckAbilityThatHasEnded(const FInstanceDataType* InstanceData, const FAbilityEndedData& AbilityEndedData) const;
+	
+};
