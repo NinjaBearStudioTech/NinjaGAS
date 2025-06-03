abedd9d1456189a69dd6b3623fcaee33a179fcf4
Thiago Uriel <thiago.uriel@gmail.com>
2025-06-03 13:19:17 -0700

    [added] New State Tree Consideration to provide utility scores based on Ability Cooldowns.

---
+++ Source/NinjaGAS/Private/AI/StateTree/StateTreeAbilityCooldownConsideration.cpp
@@ -0,0 +1,64 @@
+﻿// Ninja Bear Studio Inc., all rights reserved.
+#include "AI/StateTree/StateTreeAbilityCooldownConsideration.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "StateTreeExecutionContext.h"
+
+float FStateTreeAbilityCooldownConsideration::GetScore(FStateTreeExecutionContext& Context) const
+{
+	float Score = Super::GetScore(Context);
+
+	const UObject* Owner = Context.GetOwner();
+	const UAbilitySystemComponent* AbilitySystemComponent = GetAbilitySystemComponent(Owner);
+
+	if (IsValid(AbilitySystemComponent))
+	{
+		const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+		const FGameplayTagContainer AbilityCooldownTags = InstanceData.AbilityCooldownTags;
+
+		if (IsCooldownActive(AbilitySystemComponent, AbilityCooldownTags))
+		{
+			Score = InstanceData.ScoreWhenOnCooldown;
+		}
+		else
+		{
+			Score = InstanceData.ScoreWhenAvailable;
+		}
+	}
+	
+	return Score;
+}
+
+UAbilitySystemComponent* FStateTreeAbilityCooldownConsideration::GetAbilitySystemComponent(const UObject* Owner)
+{
+	if (!IsValid(Owner))
+	{
+		return nullptr;
+	}
+
+	if (Owner->Implements<UAbilitySystemInterface>())
+	{
+		return Cast<IAbilitySystemInterface>(Owner)->GetAbilitySystemComponent();
+	}
+
+	if (Owner->IsA<AController>())
+	{
+		const APawn* PawnOwner = Cast<AController>(Owner)->GetPawn();
+		return GetAbilitySystemComponent(PawnOwner); 
+	}
+
+	return nullptr;
+}
+
+bool FStateTreeAbilityCooldownConsideration::IsCooldownActive(const UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTagContainer& AbilityCooldownTags)
+{
+	if (!IsValid(AbilitySystemComponent) || !AbilityCooldownTags.IsValid())
+	{
+		return false;
+	}
+
+	const FGameplayEffectQuery Query = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(AbilityCooldownTags);
+	const TArray<FActiveGameplayEffectHandle> ActiveEffects = AbilitySystemComponent->GetActiveEffects(Query);
+	return ActiveEffects.Num() > 0;
+}
---
+++ Source/NinjaGAS/Public/AI/StateTree/StateTreeAbilityCooldownConsideration.h
@@ -0,0 +1,49 @@
+﻿// Ninja Bear Studio Inc., all rights reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeConsiderationBase.h"
+#include "StateTreeAbilityCooldownConsideration.generated.h"
+
+class UAbilitySystemComponent;
+
+USTRUCT()
+struct FStateTreeAbilityCooldownConsiderationInstanceData
+{
+	
+	GENERATED_BODY()
+
+	/** Gameplay tags used to identify the desired cooldown. */
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	FGameplayTagContainer AbilityCooldownTags = FGameplayTagContainer::EmptyContainer;
+
+	/** Score applied when the ability is available. */
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	float ScoreWhenAvailable = 1.f;
+	
+	/** Score applied when the ability is on cooldown. */
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	float ScoreWhenOnCooldown = 0.f;
+	
+};
+
+/**
+ * Provides the utility value based on the ability cooldown.
+ */
+USTRUCT(DisplayName = "Ability Cooldown", Category = "G.A.S.")
+struct NINJAGAS_API FStateTreeAbilityCooldownConsideration : public FStateTreeConsiderationCommonBase
+{
+	
+	GENERATED_BODY()
+
+	using FInstanceDataType = FStateTreeAbilityCooldownConsiderationInstanceData;
+	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+
+protected:
+	
+	virtual float GetScore(FStateTreeExecutionContext& Context) const override;
+
+	static UAbilitySystemComponent* GetAbilitySystemComponent(const UObject* Owner);
+	static bool IsCooldownActive(const UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTagContainer& AbilityCooldownTags); 
+	
+};
